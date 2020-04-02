f2ee941706cf0a516aa0ea6e3c9aad880c173a7a
Mohamed Soltane <soltane.mohamed.achref@gmail.com>
2020-04-02 18:09:38 -0500

    set up from Template

---
+++ client/src/components/Config.js
@@ -0,0 +1,14 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'd1b15f11c6d25f22d20da46abab68c08';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+
+
+
+
+
+
